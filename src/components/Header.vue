543c4c79cbe86f3a1163599f7d76ad6a503efce8
Jack Veiga <hello@jackveiga.com>
2019-05-28 21:54:19 +0800

    Change image size for header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,7 @@
           <Button buttonCopy="Create your Union" />
         </div>
       </div>
-      <div class="w-1/2 px-12">
+      <div class="w-1/2 max-w-xl px-12">
         <!-- <img :src="coverImageURL" alt="" /> -->
         <img src="https://previews.dropbox.com/p/thumb/AAfjwRYrsrT5HfvfHthL7m7s8OlTP-AMRbTLkj6eDWLGBAhTdEJubNr0YCmAQkDVnL9BbFGJSJYns28t04h_wvriZKtTenbmXLLUUt9JCGAN16-nwfLfIaARYQK8sch0ckVBCzAJbEzQGl1jducx11L7nmjgh64VB_ZTudrtEMijb4rF43pQrgAc3kpRQN5MZabjgBVwmZzLhfkfXh8-Y2CLBQmJqDT98kyGM4WNbhqopaK6HkT7_bE-IBVn0b2vXjyqB7KK1lTcE7sr1XgISp5iXYgcG64xQ9oRJJ_2M7MXM9k-PAo0Eu23n7lcvO98nzhM-j9kVV-WlWnbKjGYvM7d/p.png" alt="" />
       </div>
