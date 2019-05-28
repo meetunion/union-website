5f9dc8710044c0ad5bdc2c3c66075ac71a647a0c
Jack Veiga <hello@jackveiga.com>
2019-05-28 21:53:51 +0800

    Changes to card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <div class="mt-2 flex flex-col max-w-sm rounded-lg overflow-hidden shadow-lg"
+  <div class="flex flex-row items-center content-center self-center justify-center">
+    <div class="mr-12 flex flex-col max-w-sm rounded-lg overflow-hidden shadow-lg"
       v-for="item in cardItems"
       :key="item.cardId"
       >
       <div class="px-6 py-6">
-          <div class="font-bold text-xl mb-2">{{ item.cardTitle }}</div>
+          <div class="font-bold text-xl mb-3">{{ item.cardTitle }}</div>
           <p class="text-gray-700 text-base leading-normal">{{ item.cardDescription }}</p>
       </div>
     </div>
