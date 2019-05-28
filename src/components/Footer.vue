452756800d205a16710d0ce563204216e73f04b7
Jack Veiga <hello@jackveiga.com>
2019-05-28 23:06:44 +0800

    Change footer links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    <div>
+  <div class="mt-16 pb-16 p-8">
+    <div class="container mx-auto text-left opacity-75 text-sm lg:px-4">
+      <div
+        class="flex flex-col justify-center items-start content-start md:flex-row mb-4"
+      >
+        <div class="sm:w-1/2 h-auto">
       <p>Union is da bommmbbbb!!</p>
     </div>
-    <div>
+    <div class="flex flex-col sm:w-1/2 h-auto sm:mt-0 mt-8">
       <p>Connect</p>
-      <a>{{ twitterUrl }}</a>
-      <a>{{ discordUrl }}</a>
+      <a :href="twitterUrl">Twitter</a>
+      <a :href="discordUrl">Discord</a>
     </div>
+  </div>
+      </div>
   </div>
 </template>
 
